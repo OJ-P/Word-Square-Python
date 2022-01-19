268e84ae41dd108393468acb6ddfe289f100ca15
Owen JP <owenjp311@gmail.com>
2022-01-19 18:01:06 +0000

    commented user input functions

--- run_word_square.py
+++ run_word_square.py
@@ -27,31 +27,36 @@ def instructions():  # prints instructions on how program functions
     print(" Example input: 4 aaccdeeeemmnnnoo\n")
 
 
-def run_word_square():
-    sanitised_input = get_user_input()
-    print("sanitised", sanitised_input)
+def run_word_square():  # creates word square
+    valid_user_input = get_user_input()
 
 
-def get_user_input():
-    while True:
+def get_user_input():  # asks user for user input and formats it
+    while True:  # Loop until input valid
         print("Enter value to create word square:\n")
-        user_input = str(input().replace(" ", "").lower())
-        str_length = len(user_input) - 1
-        if not user_input[0].isdigit():
-            print("Invalid input - the first character must be a number")
-            break
-        elif int(user_input[0]) == 0:
-            print("Invalid input - the first character can not be zero")
-            break
-        elif (int(user_input[0]) ** 2) != str_length:
-            print("Invalid input - input length must equal first character (number) squared")
-            break
-        for current_char in user_input[1:str_length:1]:
-            if not current_char.isalpha():
-                print("Invalid input - all characters after initial number must be letters")
-                break
-            elif current_char == user_input[str_length]:
-                return user_input
+        user_input = str(input().replace(" ", "").lower())  # format to string, strip whitespace, set to lowercase
+        str_length = len(user_input) - 1  # set variable to length of inputted string
+        user_input = validate_user_input(user_input, str_length)  # call func to validate input
+        if user_input:  # if func returns true input is valid and returns value
+            return user_input
+
+
+def validate_user_input(user_input, str_length):  # validates if user input is usable
+    if not user_input[0].isdigit():  # checks if first character of string is a number
+        print("Invalid input - the first character must be a number")
+        return False
+    elif int(user_input[0]) == 0:  # checks if first character of string is greater than zero
+        print("Invalid input - the first character can not be zero")
+        return False
+    elif (int(user_input[0]) ** 2) != str_length:  # first character must equal the length of the string
+        print("Invalid input - input length must equal first character (number) squared")
+        return False
+    for current_char in user_input[1:str_length:1]:  # [start:end:step] iterates through input string from index[1]
+        if not current_char.isalpha():  # evaluates if current char is alphabetical
+            print("Invalid input - all characters after initial number must be letters")
+            return False
+        elif current_char == user_input[str_length]:  # if all chars are alphabetical, returns valid input
+            return True
 
 
 def exit_program():
