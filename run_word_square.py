21bebeaa230a5386d1a59bcfa3c8ae2dd89b15ba
Owen JP <owenjp311@gmail.com>
2022-01-19 17:25:42 +0000

    sanitise user input function added

--- run_word_square.py
+++ run_word_square.py
@@ -1,4 +1,5 @@
 import sys  # to allow sys.exit()
+import isdigit
 
 
 def main():  # main function - runs menu load function
@@ -14,7 +15,7 @@ def load_menu():  # takes users choice and either runs, gives instructions or qu
         elif user_selection == "2":
             run_word_square()
         elif user_selection == "3":
-            sys.exit()
+            exit_program()
         else:
             print("\ninvalid input")
 
@@ -27,7 +28,34 @@ def instructions():  # prints instructions on how program functions
 
 
 def run_word_square():
-    print("")
+    sanitised_input = get_user_input()
+    print("sanitised", sanitised_input)
+
+
+def get_user_input():
+    while True:
+        print("Enter value to create word square:\n")
+        user_input = str(input().replace(" ", "").lower())
+        str_length = len(user_input) - 1
+        if not user_input[0].isdigit():
+            print("Invalid input - the first character must be a number")
+            break
+        elif int(user_input[0]) == 0:
+            print("Invalid input - the first character can not be zero")
+            break
+        elif (int(user_input[0]) ** 2) != str_length:
+            print("Invalid input - input length must equal first character (number) squared")
+            break
+        for current_char in user_input[1:str_length:1]:
+            if not current_char.isalpha():
+                print("Invalid input - all characters after initial number must be letters")
+                break
+            elif current_char == user_input[str_length]:
+                return user_input
+
+
+def exit_program():
+    sys.exit()
 
 
 main()
